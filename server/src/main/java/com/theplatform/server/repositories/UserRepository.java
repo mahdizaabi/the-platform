30ccd3b354a5e31a917840f958d1a19f54f3df1d
mahdizaabi <mahdi.zaabi@gmail.com>
2021-09-07 02:36:38 +0200

    add main repositories

---
+++ server/src/main/java/com/theplatform/server/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.theplatform.server.repositories;
+
+import com.theplatform.server.models.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    public User findByUsername(String username);
+}
