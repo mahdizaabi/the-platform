a7e4a7b43d0a427c30297d25ab5b77523c55b3f9
mahdizaabi <mahdi.zaabi@gmail.com>
2021-09-07 02:32:58 +0200

    implement the main user controller

---
+++ server/src/main/java/com/theplatform/server/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.theplatform.server.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping(path = "/hello")
+    public String hello() {
+        return "hello";
+    }
+
+
+
+}
