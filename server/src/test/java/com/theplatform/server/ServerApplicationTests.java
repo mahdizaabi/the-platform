fe03a4d90ed9b6be79119224518b44cb907017cc
mahdizaabi <mahdi.zaabi@gmail.com>
2021-09-07 14:20:32 +0200

    update dependencies

--- server/src/test/java/com/theplatform/server/ServerApplicationTests.java
+++ server/src/test/java/com/theplatform/server/ServerApplicationTests.java
@@ -2,8 +2,10 @@ package com.theplatform.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootTest
+@Profile(value = "dev")
 class ServerApplicationTests {
 
 	@Test
