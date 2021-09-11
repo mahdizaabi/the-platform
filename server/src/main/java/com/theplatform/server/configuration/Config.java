8122f9508979720368cac65c2c61b871523488ef
mahdizaabi <mahdi.zaabi@gmail.com>
2021-09-11 21:02:04 +0200

    update configuration

--- server/src/main/java/com/theplatform/server/configuration/Config.java
+++ server/src/main/java/com/theplatform/server/configuration/Config.java
@@ -1,5 +1,6 @@
 package com.theplatform.server.configuration;
 
+import com.github.slugify.Slugify;
 import net.sargue.mailgun.Configuration;
 import org.springframework.context.annotation.Bean;
 
@@ -12,5 +13,11 @@ public class Config {
                 .domain("mahdi-zaabi.engineer")
                 .apiKey("key-d97ca6f87ceba17d030593266e89439f")
                 .from("Test account", "postmaster@somedomain.com");
+    }
+
+    @Bean
+    public Slugify slugify() {
+        Slugify slg = new Slugify();
+        return slg;
     }
 }
