1f63d18aa4a09827e6d04c9406d405f0bd8d06c9
mahdizaabi <mahdi.zaabi@gmail.com>
2021-09-13 12:49:49 +0200

    fix broken test

--- server/src/test/java/com/theplatform/server/ServerApplicationTests.java
+++ server/src/test/java/com/theplatform/server/ServerApplicationTests.java
@@ -5,7 +5,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
 
 @SpringBootTest
-@Profile(value = "test")
 class ServerApplicationTests {
 
 	@Test
