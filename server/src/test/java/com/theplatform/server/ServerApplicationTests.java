d8ca482b51cd5d4e3869e45e93f3a48428d8a509
mahdizaabi <mahdi.zaabi@gmail.com>
2021-09-15 20:25:04 +0200

    fix broken tests

--- server/src/test/java/com/theplatform/server/ServerApplicationTests.java
+++ server/src/test/java/com/theplatform/server/ServerApplicationTests.java
@@ -3,8 +3,11 @@ package com.theplatform.server;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @SpringBootTest
+@ContextConfiguration(classes = ServerApplicationTests.class)
 class ServerApplicationTests {
 
 	@Test
