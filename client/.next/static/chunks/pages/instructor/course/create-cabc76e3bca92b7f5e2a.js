301d1d5414dd8ce6ef157291aad2bcd437557b8f
root <root@vagrant.vm>
2021-09-17 20:00:23 +0000

    remove

--- client/.next/static/chunks/pages/instructor/course/create-cabc76e3bca92b7f5e2a.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[63],{7894:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/instructor/course/create",function(){return _(9778)}])}},function(n){n.O(0,[577,576,860,391,778,774,888,179],(function(){return u=7894,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
